1af053b00bd530839150671742fe53151c25c505
Nichibotsu <nbesecke05@gmail.com>
2025-06-20 01:14:59 +0200

    Beispiel Bild funktion

--- src/layout.py
+++ src/layout.py
@@ -3,16 +3,25 @@ import copy
 
 from .methoden import gradcam ,feature_viz
 from . import update
+from .Example_Image import load_example_image  # Funktion zum Laden des Beispielbildes
+
 
 _METHODS = ["Grad-CAM", "Saliency Map", "LIME", "Feature Viz"]
+
+
 
 
 def render_main_layout(model_name: str, model_obj):
     st.header(f"Modell: {model_name}")
     image = update.upload_image()
-    if image is None:
-        st.info("Bitte lade in der Seitenleiste ein Bild hoch.")
+     # Button zum Laden des Beispielbildes
+    if st.button("Beispielbild laden"):
+        image = load_example_image()
+        st.image(image, caption="Beagle Hund (Beispielbild)", use_column_width=True)
+    elif image is None:
+        st.info("Bitte lade in der Seitenleiste ein Bild hoch oder benutze den Button zum Laden des Beispielbildes.")
         return
+
 
     tabs = st.tabs(_METHODS)
     for tab, method in zip(tabs, _METHODS):
